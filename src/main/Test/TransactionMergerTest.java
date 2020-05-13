87cf5b4f6bda3baf4e6cca7a3a69ead4a2f0c2e0
yakrash <yakrash@ya.ru>
2020-05-14 00:29:53 +0300

    update TransactionMergerTest

--- src/main/Test/TransactionMergerTest.java
+++ src/main/Test/TransactionMergerTest.java
@@ -7,34 +7,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionMergerTest {
-    private List<FinancialTransaction> mergeList;
+    private List<FinancialTransaction> mergeList = new ArrayList<>();
     private final List<FinancialTransaction> testList = new ArrayList<>();
 
     @Test
-    public void validWorkingMerge() {
-        FinancialTransaction test1 = new FinancialTransaction(1, 2, 10);
+    public void validWorkingMerge2() {
         testList.add(new FinancialTransaction(1, 2, 10));
         testList.add(new FinancialTransaction(1, 2, 50));
         testList.add(new FinancialTransaction(1, 2, 40));
-
-        FinancialTransaction test2 = new FinancialTransaction(1, 3, 10);
         testList.add(new FinancialTransaction(1, 3, 10));
         testList.add(new FinancialTransaction(1, 3, 10));
-
-        FinancialTransaction test3 = new FinancialTransaction(2, 1, 10);
         testList.add(new FinancialTransaction(2, 1, 10));
         testList.add(new FinancialTransaction(2, 1, 50));
         testList.add(new FinancialTransaction(2, 1, 40));
-
-        FinancialTransaction test4 = new FinancialTransaction(3, 2, 10);
         testList.add(new FinancialTransaction(3, 2, 10));
         testList.add(new FinancialTransaction(1, 3, 50));
 
         mergeList = TransactionMerger.merge(testList);
 
-        Assert.assertEquals(mergeList.get(mergeList.indexOf(test1)).getAmount(), 100.0, 1e-9);
-        Assert.assertEquals(mergeList.get(mergeList.indexOf(test2)).getAmount(), 70.0, 1e-9);
-        Assert.assertEquals(mergeList.get(mergeList.indexOf(test3)).getAmount(), 100.0, 1e-9);
-        Assert.assertEquals(mergeList.get(mergeList.indexOf(test4)).getAmount(), 10.0, 1e-9);
+        Assert.assertEquals(mergeList.get(0).getAmount(), 100.0, 1e-9);
+        Assert.assertEquals(mergeList.get(1).getAmount(), 100.0, 1e-9);
+        Assert.assertEquals(mergeList.get(2).getAmount(), 70.0, 1e-9);
+        Assert.assertEquals(mergeList.get(3).getAmount(), 10.0, 1e-9);
     }
+
+
 }
