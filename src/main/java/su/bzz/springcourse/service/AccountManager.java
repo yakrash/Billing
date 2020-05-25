dc7b66aa18a7a7091cc3b62f2cafc08411bab380
yakrash <yakrash@ya.ru>
2020-05-25 18:43:51 +0300

    AccountManager delete modify & changeAmount method

--- src/main/java/su/bzz/springcourse/service/AccountManager.java
+++ src/main/java/su/bzz/springcourse/service/AccountManager.java
@@ -22,37 +22,23 @@ public class AccountManager {
     @Value("${accountManager.expireAfterAccessTimeUnit}")
     private String expireAfterAccessTimeUnit;
 
-    public AccountManager(PostgreAccountsDAO postgreAccountsDAO) {
-        this.postgreAccountsDAO = postgreAccountsDAO;
-    }
-
     private final LoadingCache<Long, Account> accounts = CacheBuilder.newBuilder()
             .expireAfterAccess(expireAfterAccessDuration, TimeUnit.valueOf(expireAfterAccessTimeUnit))
             .maximumSize(100)
             .build(new CacheLoader<Long, Account>() {
                 @Override
                 public Account load(Long id) throws Exception {
-                    return getAccountById(id);
+                    return postgreAccountsDAO.get(id);
                 }
             });
 
-    public Account getAccountById(long id) throws ExecutionException {
-        return postgreAccountsDAO.get(id);
+    public AccountManager(PostgreAccountsDAO postgreAccountsDAO) {
+        this.postgreAccountsDAO = postgreAccountsDAO;
     }
 
     public void createAccount(double amount) {
         long createId = postgreAccountsDAO.create(amount);
         accounts.put(createId, new Account(createId, amount));
-    }
-
-    public void modify(long id, double newAmount) throws ExecutionException {
-        accounts.get(id);
-        accounts.put(id, new Account(id, newAmount));
-    }
-
-    public void changeAmount(long id, double amount) throws ExecutionException {
-        Account acc = getAccountById(id);
-        acc.setAmount(acc.getAmount() + amount);
     }
 
 
