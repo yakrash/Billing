186ecbcedee1edd2776cfa74af083a6dc027d2bd
Aleksandr <yakrash@ya.ru>
2020-06-01 21:23:35 +0300

    add modify to TransactionLogger (#15)
    * add modify to TransactionLogger
    
    * fix

--- src/main/java/su/bzz/springcourse/service/TransactionLogger.java
+++ src/main/java/su/bzz/springcourse/service/TransactionLogger.java
@@ -13,6 +13,7 @@ import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
@@ -25,11 +26,14 @@ public class TransactionLogger {
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private final PostgresLoggerDAO postgresLoggerDAO;
     private static final Logger LOGGER = LoggerFactory.getLogger(TransactionLogger.class);
+    private final AccountManager accountManager;
+
+    public TransactionLogger(PostgresLoggerDAO postgresLoggerDAO, AccountManager accountManager) {
+        this.postgresLoggerDAO = postgresLoggerDAO;
+        this.accountManager = accountManager;
+    }
 
     @Autowired
-    public TransactionLogger(PostgresLoggerDAO postgresLoggerDAO) {
-        this.postgresLoggerDAO = postgresLoggerDAO;
-    }
 
     public BlockingQueue<FinancialTransaction> getLoggerFinancialTransaction() {
         return loggerFinancialTransaction;
@@ -53,8 +57,15 @@ public class TransactionLogger {
             LOGGER.info("2. MergeTempFT: " + tempFinancialTransaction);
 
             postgresLoggerDAO.insert(tempFinancialTransaction);
-            mergedFinancialTransactions.addAll(tempFinancialTransaction);
-            LOGGER.info("3. MergedFT" + mergedFinancialTransactions);
+//            mergedFinancialTransactions.addAll(tempFinancialTransaction);
+//            LOGGER.info("3. MergedFT" + mergedFinancialTransactions);
+            try {
+                for (FinancialTransaction ft : tempFinancialTransaction) {
+                    accountManager.modify(ft);
+                }
+            } catch (ExecutionException e) {
+                LOGGER.warn(e.toString());
+            }
 
         }), 0, 8, TimeUnit.SECONDS);
     }
