f0b8a95f6c91b1d674acb9fa1d87818ff6edf597
yakrash <kraash@bk.ru>
2020-10-17 14:59:26 +0300

    add AccountManagerTest

--- src/test/service/AccountManagerTest.java
+++ src/test/service/AccountManagerTest.java
@@ -12,11 +12,14 @@ import su.bzz.springcourse.service.AccountManager;
 
 import javax.sql.DataSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class AccountManagerTest {
-    AccountManager accountManager;
+    private static AccountManager accountManager;
 
     @BeforeClass
-    public void beforeClass() {
+    public static void beforeClass() {
         DataSource dataSource = new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("classpath:accounts.sql")
@@ -25,13 +28,30 @@ public class AccountManagerTest {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         PostgreAccountsDAO postgreAccountsDAO = new PostgreAccountsDAO(jdbcTemplate);
         accountManager = new AccountManager(postgreAccountsDAO);
-
+        accountManager.createAccount();
+        accountManager.createAccount();
+        accountManager.createAccount();
     }
 
     @Test
     public void methodModifyCheckSum() {
         FinancialTransaction financialTransaction = new FinancialTransaction(1, 2, 100);
-        Account account1 = new Account(1, 10, 20);
+        Account account1AfterModify = new Account(1, 0, 100);
+        Account account2AfterModify = new Account(2, 100, 0);
 
+        accountManager.modify(financialTransaction);
+        assertEquals(account1AfterModify, accountManager.getAccount(1));
+        assertEquals(account2AfterModify, accountManager.getAccount(2));
+    }
+
+    @Test
+    public void methodGetTest() {
+        Account account = new Account(3, 0, 0);
+        assertEquals(account, accountManager.getAccount(3));
+    }
+
+    @Test
+    public void methodGetWhenAccountNull() {
+        assertNull(accountManager.getAccount(99));
     }
 }
